4d8873829cf3471b89c784847849b825c6a4c855
RiTHiKA665 <rviswana@student.gitam.edu>
2024-11-23 12:54:49 +0530

    Create Fibonocci.c

---
+++ Fibonocci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int first=0,second=1,n,i,third;
+printf("no.of terms:");
+scanf("%d",&n);
+
+  printf("fibinocci series is ");
+  
+//printing of first and second numbers
+  
+  printf("%d,%d,",first,second);
+  
+//printing of fibin9cci series
+  
+ for(i=2;i<n;i++){
+  third=first +second;
+  first=second;
+  second=third;
+  printf("%d,",third);
+  }
+  
+return 0;
+}
+
+
+
+  //output is
+  no.of terms:5
+fibinocci series is 0,1,1,2,3
+[Process completed - press Enter]
