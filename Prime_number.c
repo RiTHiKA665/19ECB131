51efde96b6419cb1ca53d788787f9fe548a84aac
RiTHiKA665 <rviswana@student.gitam.edu>
2024-11-24 18:03:30 +0530

    Create Prime_number.c

---
+++ Prime_number.c
@@ -0,0 +1,19 @@
+//to verify the given number is a prime number or not
+#include <stdio.h>
+int main (){
+    int i, n, count=0;
+    printf("enter a number = ");
+    scanf("%d", &n);
+    i = 1;
+while(i<=n){
+    if(n % i ==0)
+    count++;
+    i++;
+}
+if (count == 2){
+    printf("The given number is a prime number");
+
+}
+else
+    printf("The given number is not a prime number");
+}
