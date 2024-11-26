d4b3bd70a6bc7bc4d428d08c3239ddad14fa16c8
RiTHiKA665 <rviswana@student.gitam.edu>
2024-11-26 20:14:20 +0530

    Update Function_practice2.c

--- Function_practice2.c
+++ Function_practice2.c
@@ -14,3 +14,15 @@ void table(int a){
         printf("%d * %d = %d\n", a, i, a*i);
     }
 }
+/* OUTPUT
+Enter the value of a: 5
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50 */
